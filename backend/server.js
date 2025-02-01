e049f5e8d500cfb52687003b296e027d747438a5
chandini1915 <chandini.s@gaiansolutions.com>
2025-02-01 10:49:32 +0530

    env

--- backend/server.js
+++ backend/server.js
@@ -3,7 +3,7 @@ const cors = require("cors");
 const routes = require("./routes/routes.js");
 
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 app.use(cors());
 app.use(express.json());
